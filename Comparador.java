5abd783f2813c749938ab4991b79fc60b9174429
heribertoalexandre <heriberto.alexandre@gmail.com>
2018-02-05 16:48:19 -0200

    Add files via upload
    Material academia1

---
+++ Comparador.java
@@ -0,0 +1,13 @@
+package dia1TrueWind;
+
+import java.util.Comparator;
+
+public class Comparador  implements Comparator<Integer> {
+
+		
+		@Override
+		public int compare(Integer a, Integer b) {
+			
+			return a - b;
+		}
+}
